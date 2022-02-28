d9529c3b0b6d69ae935551e65e4dca87657ef8b1
MoonsukJi93 <100415746+MoonsukJi93@users.noreply.github.com>
2022-02-27 22:11:17 -0800

    Create 14_Marketing_Campaign_Success_Advanced.sql

---
+++ 14_Marketing_Campaign_Success_Advanced.sql
@@ -0,0 +1,4 @@
+-- URL: https://platform.stratascratch.com/coding/514-marketing-campaign-success-advanced?python&utm_source=youtube&utm_medium=click&utm_campaign=YT+description+link
+-- YouTube: https://www.youtube.com/watch?v=VYeevsVj4fU&list=PLv6MQO1Zzdmq5w4YkdkWyW8AaWatSQ0kX&index=14
+
+-- My Solution:
